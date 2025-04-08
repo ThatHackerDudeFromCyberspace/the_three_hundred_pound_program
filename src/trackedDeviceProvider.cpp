78f981d6e17b57e8417e06318e82ef8beb1a46e6
micheal65536 <>
2025-04-08 17:37:19 +0100

    Removed unecessary stuff

--- src/trackedDeviceProvider.cpp
+++ src/trackedDeviceProvider.cpp
@@ -1,31 +1,20 @@
 #include "trackedDeviceProvider.h"
 #include "controllerDevice.h"
-#include "globalState.h"
-#include "hmdDevice.h"
 #include "openvr_driver.h"
 
 vr::EVRInitError TrackedDeviceProvider::Init(vr::IVRDriverContext* pDriverContext) {
     VR_INIT_SERVER_DRIVER_CONTEXT(pDriverContext);
 
-    hmdDevice = new HMDDevice();
     leftController = new ControllerDevice( vr::TrackedControllerRole_LeftHand );
     rightController = new ControllerDevice( vr::TrackedControllerRole_RightHand );
 
-    // Add HMD
-    vr::VRServerDriverHost()->TrackedDeviceAdded("300HMD", vr::TrackedDeviceClass_HMD, hmdDevice);
     vr::VRServerDriverHost()->TrackedDeviceAdded("300Controller_Left", vr::TrackedDeviceClass_Controller, leftController);
     vr::VRServerDriverHost()->TrackedDeviceAdded("300Controller_Right", vr::TrackedDeviceClass_Controller, rightController);
 
-    // Add two controllers @TODO
-
-    GlobalState::GetGlobalState()->frameNumber = 0;
     return vr::VRInitError_None;
 }
 
 void TrackedDeviceProvider::Cleanup() {
-    delete hmdDevice;
-    hmdDevice = NULL;
-
     delete leftController;
     leftController = NULL;
 
@@ -34,8 +23,6 @@ void TrackedDeviceProvider::Cleanup() {
 }
 
 void TrackedDeviceProvider::RunFrame() {
-    GlobalState::GetGlobalState()->frameNumber++;
-
     //Now, process events that were submitted for this frame.
 	vr::VREvent_t vrevent{};
 	while ( vr::VRServerDriverHost()->PollNextEvent( &vrevent, sizeof( vr::VREvent_t ) ) )
